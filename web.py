c384ebfed69104391efdc65a7a5bc0b27faa0b4e
ilan <ilan@ilanponimansky.com>
2017-10-09 00:32:19 -0400

    adding user agent test

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return 'Hello World in Flask. Your User Agent is {}'.format(request.headers.get('User-Agent'))
+    return 'Your User Agent is {}'.format(request.headers.get('User-Agent'))
 
 
 if __name__ == "__main__":
